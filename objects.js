ce677c4162120ab7f1430ad63b8633f7f1e71746
Ankur Puri <apuri@iit.edu>
2018-05-20 05:14:28 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,5 +1,6 @@
 var playlist = {
   artist: 'song'
+<<<<<<< HEAD
 }
 
 function updatePlaylist(playlist, artistName, songTitle) {
@@ -10,4 +11,6 @@ function updatePlaylist(playlist, artistName, songTitle) {
 function removeFromPlaylist(playlist, artistName) {
   delete playlist[artistName]
   return  playlist
+=======
+>>>>>>> fe85c3d90bf6c2428e83765b3c4cc8c220eef07e
 }
